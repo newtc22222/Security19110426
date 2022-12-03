b201245ede1ca460f1236331cb12165b77704bb0
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2022-12-04 01:19:41 +0700

    update https, helmet

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
 
-app.get("/secret", (req, res) => {
+app.use(helmet());
+
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true;
+    if(!isLoggedIn) {
+        return res.status(401).json({
+            error: 'You must log in!',
+        });
+    }
+    next();
+}
+
+app.get('/auth/google', (req, res) => {});
+
+app.get('/auth/google/callback', (req, res) => {});
+
+app.get('/auth/logout', (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
     return res.send("Your personal secret value is 21!");
 });
 
@@ -13,6 +34,9 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}...`);
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 });
